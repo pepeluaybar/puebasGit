9885469a02584c4f033add93cf8daa3a55787ae0
unknown <joseluis.aybar.gutierrez@gmail.com>
2025-11-05 19:21:40 +0100

    adios mundo

--- pruebasGit/src/pruebasgit/PruebasGit.java
+++ pruebasGit/src/pruebasgit/PruebasGit.java
@@ -15,6 +15,7 @@ public class PruebasGit {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Adios mundo");
     }
     
 }
